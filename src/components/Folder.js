bfc83579762543c6667cfd57bedc4f274fc1edbd
mateomusi <mateomusi657@gmail.com>
2023-12-04 16:14:31 -0300

    modifico los botones

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -86,10 +86,12 @@ const Folder = ({
           <button onClick={handleMostrarCampoNombre}>Agregar Carpeta</button>
         )}
         <button className="eliminar" onClick={handleEliminarCarpeta}>
-          <FontAwesomeIcon icon={faX} size="lg" style={{ color: "#000000" }} />
+          <svg viewBox="0 0 448 512" className="svgIcon">
+            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+          </svg>
         </button>
       </div>
-      <div>
+      <div className="formulario">
         {/* Formulario para agregar una nueva transacción */}
         <input
           type="date"
@@ -122,20 +124,22 @@ const Folder = ({
             })
           }
         />
-        <button
-          className="pago"
-          style={{ backgroundColor: "#ff0400" }}
-          onClick={() => handleTipoTransaccion("Pago")}
-        >
-          P
-        </button>
-        <button
-          className="cobro"
-          style={{ backgroundColor: "#28ed1d" }}
-          onClick={() => handleTipoTransaccion("Cobro")}
-        >
-          C
-        </button>
+        <div className="btn-pc">
+          <button
+            className="pago"
+            style={{ backgroundColor: "#ff0400" }}
+            onClick={() => handleTipoTransaccion("Pago")}
+          >
+            Pago
+          </button>
+          <button
+            className="cobro"
+            style={{ backgroundColor: "#28ed1d" }}
+            onClick={() => handleTipoTransaccion("Cobro")}
+          >
+            Cobro
+          </button>
+        </div>
       </div>
       <button className="agregar" onClick={handleAgregarTransaccion}>
         Agregar Transacción
